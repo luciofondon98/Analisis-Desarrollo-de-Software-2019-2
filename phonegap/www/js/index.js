366c1f87917f28b0f967fc9ff6172b379c5bad6a
lfondon <lucio.fondon@sansano.usm.cl>
2020-01-07 00:25:13 -0300

    agregando front consultas

--- phonegap/www/js/index.js
+++ phonegap/www/js/index.js
@@ -1,10 +1,10 @@
-var idCliente = 0;
+var id = 0;
 var url_inicio = "";
 var url_api = "";
 
 $(document).ready(function() {
     $("#botoningresar").click(function (e) {
-
+        e.preventDefault();
         let tipo_usuario = document.getElementById("tipo_usuario");
         console.log(tipo_usuario.value);
         if(tipo_usuario.value=="Usuario"){
@@ -18,36 +18,57 @@ $(document).ready(function() {
             console.log("si");
 
             url_api = "http://localhost:8000/v1/tecnico/getAll";
-            url_inicio = "/inicio_tecnico.html"
+           
         
         }
-        e.preventDefault();
-        $.ajax({
-            type: "GET",
-            url: url_api,
-            success: function(data) {
-                console.log("si");
-                let pass = document.getElementById("contrasena_usuario");
-                let correo = document.getElementById("email_usuario");
-                for (let i = 0; i < data.length; i++){ 
-                    if (data[i].contrasena == pass.value){
-                        if(data[i].correo == correo.value){
-                            console.log(data[i].id)
-                            idCliente = data[i].id;
-                        }
-                    }              
-                }
-                if(idCliente == 0) {
-                    alert("Cliente no registrado");
-                }
-                else window.location.href = url_inicio;
-            },
-            error: function() {
-                console.log("No se ha podido obtener la información");
-            }
-        });
-        return false;
     });
   });
+
+  function loguearCliente(id,email_registro, pass_registro){
+    $.ajax({				
+        type: "GET",
+        url: "http://localhost:8000/v1/cliente/getAll",
+        success: function(data) {
+            console.log("si");
+            for (let i = 0; i < data.length; i++){ 
+                if (data[i].contrasena == pass_registro){
+                    if(data[i].correo == email_registro){
+                        console.log(data[i].id)
+                        id = data[i].id;
+                    }
+                }              
+            }
+            if(id == 0) {
+                alert("Cliente no registrado");
+            }
+            else window.location.href = "/inicio_cliente.html";
+        },
+        error: function() {
+            console.log("No se ha podido obtener la información");
+        }
+    });
+  }
+
+//------------------------------------------------------------------------/
+
+
+  function registrarTecnico(idCliente,email_registro, pass_registro){
+    $.ajax({				
+        type: "GET",
+        url: "http://localhost:8000/v1/tecnico/tecnico",
+        data: JSON.stringify({"nombre": nombre_registro,"correo": email_registro,"contrasena":pass_registro}),
+        contentType: "application/json",
+        dataType: "json",
+        success: function(){
+            alert("Te haz registrado correctamente")
+            window.location.href = "/index.html"
+        },
+        failure: function() {
+            alert("error")
+        }
+    });
+  }
+
+
 
 
