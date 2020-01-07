3c620f91899c38ed2dc5f3415e95bf5196fa2962
lfondon <lucio.fondon@sansano.usm.cl>
2020-01-07 02:05:05 -0300

    avance mostrar consultas, falta arreglar detalle jquery

---
+++ phonegap/platforms/browser/www/js/consultas.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $("#mostrarconsultas").click(function (e) {
+        window.location.href = "/consultas.html";
+        e.preventDefault();
+        $.ajax({				
+            type: "GET",
+            url: "http://localhost:8000/v1/consulta/getAll",
+            success: function(data) {
+                console.log("consulta");
+                for (let i = 0; i < data.length; i++){    
+                    $("#consultas").append('                                                     \
+                    <div class="card" style="width: 18rem;">                                                       \
+                        <div class="card-body">                                              \
+                            <h5 class="card-title">Consulta '+data[i].titulo+'Hecha por el usuario de ID'+data[i].cliente+'</h5>         \
+                            <h6 class="card-subtitle mb-2 text-muted">Categoria: '+ data[i].categoria +'</h6>    \
+                            <p class="card-text">Descripción: '+ data[i].descripcion +'</p>    \
+                            <a href="#" class="card-link">Card link</a>                            \
+                            <a href="#" class="card-link">Another link</a>                         \
+                        </div>                                                               \
+                    </div>                                                                   \
+                ');    
+                }
+            },
+            error: function() {
+                console.log("No se ha podido obtener la información");
+            }
+        });
+    });
+  });
--- phonegap/platforms/browser/www/js/index.js
+++ phonegap/platforms/browser/www/js/index.js
@@ -1,38 +1,33 @@
 var id = 0;
-var url_inicio = "";
-var url_api = "";
 
 $(document).ready(function() {
     $("#botoningresar").click(function (e) {
         e.preventDefault();
         let tipo_usuario = document.getElementById("tipo_usuario");
+        var email_usuario = document.getElementById("email_usuario");
+        var pass_usuario = document.getElementById("contrasena_usuario");
         console.log(tipo_usuario.value);
         if(tipo_usuario.value=="Usuario"){
-            console.log("si");
-
-            url_api = "http://localhost:8000/v1/cliente/getAll";
-            url_inicio = "/inicio_cliente.html"
+            console.log("us");
+            loguearCliente(id,email_usuario.value,pass_usuario.value);
         } 
         
         else{
-            console.log("si");
-
-            url_api = "http://localhost:8000/v1/tecnico/getAll";
-           
-        
+            console.log("tec");
+            loguearTecnico(id,email_usuario.value,pass_usuario.value);
         }
     });
   });
 
-  function loguearCliente(id,email_registro, pass_registro){
+  function loguearCliente(id,email_usuario, contrasena_usuario){
     $.ajax({				
         type: "GET",
         url: "http://localhost:8000/v1/cliente/getAll",
         success: function(data) {
             console.log("si");
             for (let i = 0; i < data.length; i++){ 
-                if (data[i].contrasena == pass_registro){
-                    if(data[i].correo == email_registro){
+                if (data[i].contrasena == contrasena_usuario){
+                    if(data[i].correo == email_usuario){
                         console.log(data[i].id)
                         id = data[i].id;
                     }
@@ -52,19 +47,27 @@ $(document).ready(function() {
 //------------------------------------------------------------------------/
 
 
-  function registrarTecnico(idCliente,email_registro, pass_registro){
+  function loguearTecnico(id,email_usuario, contrasena_usuario){
     $.ajax({				
         type: "GET",
-        url: "http://localhost:8000/v1/tecnico/tecnico",
-        data: JSON.stringify({"nombre": nombre_registro,"correo": email_registro,"contrasena":pass_registro}),
-        contentType: "application/json",
-        dataType: "json",
-        success: function(){
-            alert("Te haz registrado correctamente")
-            window.location.href = "/index.html"
+        url: "http://localhost:8000/v1/tecnico/getAll",
+        success: function(data) {
+            console.log("si");
+            for (let i = 0; i < data.length; i++){ 
+                if (data[i].contrasena == contrasena_usuario){
+                    if(data[i].correo == email_usuario){
+                        console.log(data[i].id)
+                        id = data[i].id;
+                    }
+                }              
+            }
+            if(id == 0) {
+                alert("Tecnico no registrado");
+            }
+            else window.location.href = "/inicio_tecnico.html";
         },
-        failure: function() {
-            alert("error")
+        error: function() {
+            console.log("No se ha podido obtener la información");
         }
     });
   }
